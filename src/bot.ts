ae144291d437ccddbf37921f3f2053eae77c8923
p-estor <estorfernandezpedro@gmail.com>
2025-05-02 18:14:20 +0200

    probando nuevo comando para aprender

--- src/bot.ts
+++ src/bot.ts
@@ -152,7 +152,7 @@ client.on('interactionCreate', async (interaction) => {
     default:
       await interaction.reply({ content: 'Acción desconocida' });
   }
-});
+};
 
 
 // Crear archivo players.json si no existe
