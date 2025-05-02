49feec08267ceede208fe78faeaab3b64517eded
p-estor <estorfernandezpedro@gmail.com>
2025-05-02 18:24:47 +0200

    probando nuevo comando para aprender

--- src/commands/rango.ts
+++ src/commands/rango.ts
@@ -1,37 +1,20 @@
-import { MessageActionRow, MessageButton } from 'discord.js'
+import { CommandInteraction, MessageActionRow, MessageButton } from 'discord.js';
 
-import { CommandInterface } from '../interfaces/command.interface.js'
-import { Config } from '../interfaces/config.interface.js'
-import { I18n } from 'i18n'
-
-export default class RangoCommand extends CommandInterface<CommandInteraction> {
-  config: Config
-  i18n: I18n
-
-  constructor(config: Config, i18n: I18n) {
-    super('rango')
-    this.config = config
-    this.i18n = i18n
-  }
-
+export default {
+  name: 'rango',
+  description: 'Comando de prueba para verificar botones',
   async execute(interaction: CommandInteraction) {
-    const button = new ButtonBuilder()
+    const botonVerificar = new MessageButton()
       .setCustomId('rango_boton')
-      .setLabel('Haz clic aquí')
-      .setStyle(ButtonStyle.Primary)
+      .setLabel('Verificar icono')
+      .setStyle('PRIMARY');
 
-    const row = new ActionRowBuilder<ButtonBuilder>().addComponents(button)
+    const row = new MessageActionRow().addComponents(botonVerificar);
 
     await interaction.reply({
-      content: 'Pulsa el botón para continuar:',
-      components: [row]
-    })
-  }
-
-  getSlashCommandData() {
-    return {
-      name: 'rango',
-      description: 'Muestra un botón de prueba',
-    }
-  }
-}
+      content: 'Haz clic en el botón para comenzar la verificación:',
+      components: [row],
+      ephemeral: true,
+    });
+  },
+};
