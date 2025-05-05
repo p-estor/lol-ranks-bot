10013ae2050ce1ef6a0bcc794c8d93154007a342
p-estor <estorfernandezpedro@gmail.com>
2025-05-05 12:07:31 +0200

    probando nuevo comando para aprender

--- src/commands/rango.ts
+++ src/commands/rango.ts
@@ -1,20 +1,21 @@
 import { CommandInteraction, MessageActionRow, MessageButton } from 'discord.js';
 
-export default {
-  name: 'rango',
-  description: 'Comando de prueba para verificar botones',
+export default class RangoCommand {
+  name = 'rango';
+  description = 'Comando de prueba para verificar botones';
+
   async execute(interaction: CommandInteraction) {
     const botonVerificar = new MessageButton()
       .setCustomId('rango_boton')
       .setLabel('Verificar icono')
       .setStyle('PRIMARY');
 
-    const row = new MessageActionRow().addComponents(botonVerificar); // Esto solo funciona a partir de v13.3.0
+    const row = new MessageActionRow().addComponents(botonVerificar);
 
     await interaction.reply({
       content: 'Haz clic en el botón para comenzar la verificación:',
       components: [row],
       ephemeral: true,
     });
-  },
-};
+  }
+}
